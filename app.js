5eea7c5054fd07f9c6e0681a035ce078bef07a5b
Anup Ray <rayanup95@gmail.com>
2022-01-19 14:05:23 +0530

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const express = require('express')
-const app = express()
-
-const PORT = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("<h1>Message: Successful</h1>")
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-app.get("/products",  (req, res) => {
-    res.send("This is in products")
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
 })
 
-app.listen(PORT, () => {
-    console.log("Demo app is up and listening to port no "+PORT);
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
 })
+ 
